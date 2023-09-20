22928110837a3adcb0f93b58487477bb4c2e9bf8
anjufehno <lizakrug@tlu.ee>
2023-09-20 21:26:58 +0300

    comment

---
+++ tunnitoo/tund.js
@@ -0,0 +1,30 @@
+//let a='1'
+//console.log(+1)
+//console.log(parseInt(a))
+/*let a=2;
+let b= 99;
+[a,b]=[b,a]
+console.log(a,b)
+
+const massiiv = [1, 2, 3, 4, 47]
+let a = massiiv[4]
+console.log(a)
+
+const raimo={
+    name:"raimo",
+    school:"tlu"
+}
+
+console.log(raimo.name, raimo.school)
+
+
+const a={
+    time:1,
+    city:2,
+    house:3
+}
+
+const deconstruct = () => {
+    const{}
+}
+*/
